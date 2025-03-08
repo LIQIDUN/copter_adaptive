2a4a32451115584455403534cbde27c1b126ad1c
LIQIDUN <2826077916@qq.com>
2025-03-08 17:10:57 +0800

    实现了对于电机的正常控制
    修改了motors.cpp

--- ArduCopter/mode_geometric.cpp
+++ ArduCopter/mode_geometric.cpp
@@ -37,14 +37,14 @@ void ModeGeometric::run()
     {
     case AP_Motors::SpoolState::SHUT_DOWN:
         // Motors Stopped
-        attitude_control->reset_yaw_target_and_rate();
-        attitude_control->reset_rate_controller_I_terms();
+        // attitude_control->reset_yaw_target_and_rate();
+        // attitude_control->reset_rate_controller_I_terms();
         break;
 
     case AP_Motors::SpoolState::GROUND_IDLE:
         // Landed
-        attitude_control->reset_yaw_target_and_rate();
-        attitude_control->reset_rate_controller_I_terms_smoothly();
+        // attitude_control->reset_yaw_target_and_rate();
+        // attitude_control->reset_rate_controller_I_terms_smoothly();
         break;
 
     case AP_Motors::SpoolState::THROTTLE_UNLIMITED:
@@ -65,15 +65,15 @@ void ModeGeometric::run()
 
     VectorN<float, 4> thrustAndMomentCmd;
 
-    // 1.test:  only use attitude controller,constant attitude horizontal
-    // using joystick inputs
+    // 1.测试:  只进行姿态控制,保持水平姿态，假设角速度0
+    
     /**/
     // Matrix3f target_attitude = JoyStickToTargetAttitude();
     // thrustAndMomentCmd=ModeGeometric::geometricAttitudeController(target_attitude);
     /**/
-    // joystick command end
+    
 
-    // 2.using generated trajectory
+    // 2.使用生成的轨迹
     /**/
     static uint32_t initial_time_in_geometric = 0;
     static uint32_t last_time_in_geometric = 0;
@@ -300,14 +300,14 @@ void ModeGeometric::run()
     int8_t motorEnable = 1;
     if (motors->armed()) // only command the motor PWM when the vehicle is armed.
     {
-        // motors->rc_write(0, 1000 + motorEnable * 10 * motorPWM[0]); // manual set motor speed: PWM_MIN/MAX has been forced to 1000/2000
-        // motors->rc_write(1, 1000 + motorEnable * 10 * motorPWM[1]); // rc_write is called from <AP_Motors/AP_Motors_Class.h>
-        // motors->rc_write(2, 1000 + motorEnable * 10 * motorPWM[2]);
-        // motors->rc_write(3, 1000 + motorEnable * 10 * motorPWM[3]);
-        motors->rc_write(0,motorEnable *1150);
-        motors->rc_write(1,0);
-        motors->rc_write(2,0);
-        motors->rc_write(3,0);
+        motors->rc_write(0, 1000 + motorEnable * 10 * motorPWM[0]); // manual set motor speed: PWM_MIN/MAX has been forced to 1000/2000
+        motors->rc_write(1, 1000 + motorEnable * 10 * motorPWM[1]); // rc_write is called from <AP_Motors/AP_Motors_Class.h>
+        motors->rc_write(2, 1000 + motorEnable * 10 * motorPWM[2]);
+        motors->rc_write(3, 1000 + motorEnable * 10 * motorPWM[3]);
+        // motors->rc_write(0,motorEnable *1500);
+        // motors->rc_write(1,motorEnable *1500);
+        // motors->rc_write(2,motorEnable *1500);
+        // motors->rc_write(3,motorEnable *1500);
 
     }
     else
@@ -905,7 +905,7 @@ VectorN<float, 4> ModeGeometric::motorMixSimple(VectorN<float, 4> thrustMomentCm
     // const float b_M = 0.0059137;
 #elif (REAL_OR_SITL) // parameters for real drone
     // const float L = 0.25; // longer distance between adjacent motors
-    // const float D = 0.25; // shorter distance between adjacent motors
+    const float D = 0.25; // shorter distance between adjacent motors
     
 
     const float a_F = 0.000361;
@@ -913,7 +913,7 @@ VectorN<float, 4> ModeGeometric::motorMixSimple(VectorN<float, 4> thrustMomentCm
     // const float a_M = 0.00000503;
     // const float b_M = 0.00007975;
 #endif
-    // const float ct = 0.0480;
+    const float ct = 0.0480;
     // float thrust_total = thrustMomentCmd[0];
     // float M1 = thrustMomentCmd[1];
     // float M2 = thrustMomentCmd[2];
@@ -925,25 +925,45 @@ VectorN<float, 4> ModeGeometric::motorMixSimple(VectorN<float, 4> thrustMomentCm
     VectorN<float, 4> quad_output_mat_fm2f3;
     VectorN<float, 4> quad_output_mat_fm2f4;
 
+    // quad_output_mat_fm2f1[0] = 0.25;
+    // quad_output_mat_fm2f1[1] = 0.25;
+    // quad_output_mat_fm2f1[2] = 0.25;
+    // quad_output_mat_fm2f1[3] = 0.25;
+
+    // quad_output_mat_fm2f2[0] = 0;
+    // quad_output_mat_fm2f2[1] = -2;
+    // quad_output_mat_fm2f2[2] = 0;
+    // quad_output_mat_fm2f2[3] = 2;
+
+    // quad_output_mat_fm2f3[0] = 2;
+    // quad_output_mat_fm2f3[1] = 0;
+    // quad_output_mat_fm2f3[2] = -2;
+    // quad_output_mat_fm2f3[3] = 0;
+
+    // quad_output_mat_fm2f4[0] = -5.20833;
+    // quad_output_mat_fm2f4[1] = 5.20833;
+    // quad_output_mat_fm2f4[2] = -5.20833;
+    // quad_output_mat_fm2f4[3] = 5.20833;
+
     quad_output_mat_fm2f1[0] = 0.25;
     quad_output_mat_fm2f1[1] = 0.25;
     quad_output_mat_fm2f1[2] = 0.25;
     quad_output_mat_fm2f1[3] = 0.25;
 
-    quad_output_mat_fm2f2[0] = 0;
-    quad_output_mat_fm2f2[1] = -2;
-    quad_output_mat_fm2f2[2] = 0;
-    quad_output_mat_fm2f2[3] = 2;
+    quad_output_mat_fm2f2[0] = -0.5/D;
+    quad_output_mat_fm2f2[1] = 0.5/D;
+    quad_output_mat_fm2f2[2] = 0.5/D;
+    quad_output_mat_fm2f2[3] = -0.5/D;
 
-    quad_output_mat_fm2f3[0] = 2;
-    quad_output_mat_fm2f3[1] = 0;
-    quad_output_mat_fm2f3[2] = -2;
-    quad_output_mat_fm2f3[3] = 0;
+    quad_output_mat_fm2f3[0] = 0.5/D;
+    quad_output_mat_fm2f3[1] = -0.5/D;
+    quad_output_mat_fm2f3[2] = 0.5/D;
+    quad_output_mat_fm2f3[3] = -0.5/D;
 
-    quad_output_mat_fm2f4[0] = -5.20833;
-    quad_output_mat_fm2f4[1] = 5.20833;
-    quad_output_mat_fm2f4[2] = -5.20833;
-    quad_output_mat_fm2f4[3] = 5.20833;
+    quad_output_mat_fm2f4[0] = 0.25/ct;
+    quad_output_mat_fm2f4[1] = 0.25/ct;
+    quad_output_mat_fm2f4[2] = -0.25/ct;
+    quad_output_mat_fm2f4[3] = -0.25/ct;
 
     motor_force[0] = quad_output_mat_fm2f1[0] * thrustMomentCmd[0] + quad_output_mat_fm2f2[0] * thrustMomentCmd[1] + quad_output_mat_fm2f3[0] * thrustMomentCmd[2] + quad_output_mat_fm2f4[0] * thrustMomentCmd[3];
     motor_force[1] = quad_output_mat_fm2f1[1] * thrustMomentCmd[0] + quad_output_mat_fm2f2[1] * thrustMomentCmd[1] + quad_output_mat_fm2f3[1] * thrustMomentCmd[2] + quad_output_mat_fm2f4[1] * thrustMomentCmd[3];
@@ -952,7 +972,7 @@ VectorN<float, 4> ModeGeometric::motorMixSimple(VectorN<float, 4> thrustMomentCm
     // solve for linearizing point
     // float w0 = (-b_F + sqrtF(b_F * b_F + a_F * thrustMomentCmd[0])) / 2 / a_F;
 
-    //电机编号有问题，先等一下
+    
     motor_pwm[0] = (-b_F + sqrtF(b_F * b_F + a_F * motor_force[0]*4)) / 2 / a_F;
     motor_pwm[1] = (-b_F + sqrtF(b_F * b_F + a_F * motor_force[1]*4)) / 2 / a_F;
     motor_pwm[2] = (-b_F + sqrtF(b_F * b_F + a_F * motor_force[2]*4)) / 2 / a_F;
